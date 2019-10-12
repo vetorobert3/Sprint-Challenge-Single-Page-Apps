3b126a67da8afdb01203f6c94e53406989d8aa7d
Veto Ramirez <vetorobert3@gmail.com>
2019-10-12 09:04:04 -0700

    staring on project. Not getting a console log to shoe data.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [chars, setChars] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const setChars = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+          console.log(response);
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   }, []);
+
+  console.log('here')
 
   return (
     <section className="character-list">
